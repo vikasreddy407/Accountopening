3a731b7abffce5f15771d1d1cc98bb14ee3c47d1
vikasreddy407 <vikasreddy407@gmail.com>
2023-01-31 11:31:20 +0530

    recent changes

--- src/app/frontdesk/frontdesk.component.ts
+++ src/app/frontdesk/frontdesk.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { Taskdetails } from '../taskdetails/Taskdetails';
 import { UTask } from '../update/UTask';
@@ -17,7 +18,8 @@ export class FrontdeskComponent implements OnInit {
   msg: string;
   constructor(private http: HttpClient,
     public router: Router,
-    public activatedRoute:ActivatedRoute
+    public activatedRoute:ActivatedRoute,
+    private toastr: ToastrService
 
   ) { }
 
@@ -25,6 +27,10 @@ export class FrontdeskComponent implements OnInit {
     this.activatedRoute.params.subscribe(({taskid}) => { 
       this.getTaskbyId(taskid).subscribe((data)=>{this.tasks=data})
     })
+  }
+
+  showError(msg:string) {
+    this.toastr.error(msg, 'Error', { positionClass: 'toast-bottom-right' });
   }
   getTaskbyId(id:string): Observable<any>{
     return this.http.get("http://localhost:8080/engine-rest/task/"+id);
@@ -35,7 +41,11 @@ export class FrontdeskComponent implements OnInit {
      this.utask ={...this.utask, ...form.value};
      this.update(this.utask).subscribe((data)=>{
       console.log(this.utask);
-     })
+     },({error})=>{
+      console.log(error)
+      const  { message } = JSON.parse(error)
+      this.showError("Something Went Wrong !!!")
+    });
    }
    updateSel(value:string){
     this.utask ={...this.utask, taskStatus:value}
